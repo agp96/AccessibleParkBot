be5eefd43c98ac317a344aaadd321f042d1a82ef
agp08 <agpalmoradi@hotmail.com>
2026-03-30 20:55:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,4 @@ from keep_alive import keep_alive
 from bot import main
 
 if __name__ == "__main__":
-    keep_alive()   # Arranca el servidor Flask en hilo daemon
     main()         # Arranca el bot (bloqueante)
